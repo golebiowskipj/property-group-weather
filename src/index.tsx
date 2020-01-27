d3095c21e6408b5be5e96aa64d0efdadc65b6c2b
Piotr <piotr.golebiowski@efirst.asia>
2020-01-27 19:43:05 +0100

    cleaning, reduc, router, sass, ie9

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+import * as serviceWorker from './serviceWorker';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
