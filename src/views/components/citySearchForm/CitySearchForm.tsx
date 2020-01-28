aeb366852322af37ba03ca2b7542846edf06125b
Piotr <piotr.golebiowski@efirst.asia>
2020-01-28 21:19:34 +0100

    form handling, updating local state, validation, submit, going to another view

--- src/views/components/citySearchForm/CitySearchForm.tsx
+++ src/views/components/citySearchForm/CitySearchForm.tsx
@@ -2,19 +2,51 @@ import React, { Component, ChangeEvent } from 'react'
 import { InputField } from '../../../components/inputField/InputField';
 import styles from './styles/CitySearchForm.module.scss';
 import { SubmitButton } from '../../../components/submitButton/SubmitButton';
+import { historyHelper } from '../../../helpers/historyHelper';
 
-export class CitySearchForm extends Component {
+interface IState {
+    citySearch: string;
+    cityHasErrors: boolean;
+    formValid: boolean;
+}
+
+export class CitySearchForm extends Component<{}, IState> {
     state = {
-        citySearch: undefined,
+        citySearch: '',
+        cityHasErrors: false,
+        formValid: false,
     }
+
+    handleTextInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        // TODO: prevent from typing numbers 
+        this.setState({
+            citySearch: e.target.value,
+        }, () => {
+            this.validateForm();
+        });
+    }
+
+    cityNameValid = (): boolean => {
+        return this.state.citySearch.length >= 2;
+    }
+
+    validateForm = (): void => {
+        this.setState({ formValid: this.cityNameValid() })
+    }
+
+    showFormErrors = (): void => {
+        this.setState({ cityHasErrors: !this.cityNameValid() })
+    }
+
     handleSubmit = (e: React.FormEvent): void => {
         e.preventDefault();
-        console.log('elo')
+        this.showFormErrors();
+        
+        if (this.state.formValid) {
+            historyHelper.push(`/city/${this.state.citySearch}`);
+        }
     }
 
-    handleTextInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e);
-    }
     render() {
         return (
             <form
@@ -28,7 +60,7 @@ export class CitySearchForm extends Component {
                     placeholder='City...'
                     width='250px'
                     handleOnChange={this.handleTextInputChange}
-                    hasErrors={false}
+                    hasErrors={this.state.cityHasErrors}
                 />
                 <SubmitButton>
                     Search
